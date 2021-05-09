a57031c1863b57575712e9c0db59a4fdc1fb8e26
Lindon779 <66656018+Lindon779@users.noreply.github.com>
2021-05-09 16:52:16 +0800

    Delete resume/src directory

--- resume/src/network/request/api.js
+++
@@ -1,9 +0,0 @@
-import { get, post } from './request'
-
-export const getMessage = p => get('/client/getMessage', p);
-
-export const addMessage = p => post('/client/addMessage', p);
-
-export const addLikeNum = p => get('/client/addLikeNum', p);
-
-export const getLikeNum = p => get('/client/getLikeNum', p);
